5a8f251cb45c3ffdf76c552d8935597935bd6930
emirhanyener <yeneremirhann@gmail.com>
2022-11-05 22:25:58 +0300

    add menu page

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div class="menu">
+    <div class="menu-item">
+        <a href="index.php" style="height:100px;">
+            <img src="images/logo.png" height="100" class="" alt="logo">
+        </a>
+    </div>
+    <div class="menu-item">              
+        <a class="menu-item-right" style="color:white;background-color: #13a313;border-radius: 45px;" href="masaustu.php">Masaüstüne Git</a>
+        <a class="menu-item-right" href="kayit.php">Kayıt Ol</a>
+        <a class="menu-item-right" href="giris.php">Giriş Yap</a>
+        <a class="menu-item-right" href="index.php">Anasayfa</a>
+    </div>
+</div>
