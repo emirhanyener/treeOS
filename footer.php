8f4ed1be86c56123a38d5bc799b562c55d68e194
emirhanyener <yeneremirhann@gmail.com>
2022-11-06 23:01:50 +0300

    update footer

--- footer.php
+++ footer.php
@@ -1,1 +1,17 @@
-footer
+<div class="footer">
+    <div>
+        © 2022 treeOS
+    </div>
+    <div>
+        <a href="index.php" style="height:100px;">
+            <img src="images/logo.png" height="100" class="" alt="logo">
+        </a>
+    </div>
+        <div>
+              <a href="index.php" class="nav-link px-2 text-muted">Home</a>
+              <a href="login.html" class="nav-link px-2 text-muted">Login</a>
+              <a href="register.html" class="nav-link px-2 text-muted">Register</a>
+              <a href="desktop.html" class="nav-link px-2 text-muted">Desktop</a>
+        </div>
+    <div>
+</div>
