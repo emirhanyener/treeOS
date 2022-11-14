e0514498961d97d7aedf6a29ab9bacc23ea9d243
emirhanyener <yeneremirhann@gmail.com>
2022-11-14 17:51:37 +0300

    add: file class

---
+++ file_class.php
@@ -0,0 +1,23 @@
+<?php
+    class File {
+        public $name;
+        public $position_x;
+        public $position_y;
+
+        function __construct($name,$position_x,$position_y){
+            $this->name = $name;
+            $this->position_x = $position_x;
+            $this->position_y = $position_y;
+        }
+
+        function get_name(){
+            return $this->name;
+        }
+        function get_position_x(){
+            return $this->position_x;
+        }
+        function get_position_y(){
+            return $this->position_y;
+        }
+    }
+?>
