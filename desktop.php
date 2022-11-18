d422be6990523ba1929a9d63207aa1dae0b895fe
emirhanyener <yeneremirhann@gmail.com>
2022-11-18 22:36:08 +0300

    update desktop: login control

--- desktop.php
+++ desktop.php
@@ -3,10 +3,20 @@
     <?php
         include("head.php");
     ?>
+    <?php
+        if(isset($_SESSION["login"])){
+    ?>
     <body>
         <canvas id="application"></canvas>
     </body>
     <?php
+        } else {
+    ?>
+    <div class="container">
+        <p>For using desktop please <a href="login.php">login</a></p>
+    </div>
+    <?php
+        }
         include("js/appscript.php");
     ?>
 </html>
