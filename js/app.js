af997c411a0727684b5519fb70830f13fd7f28a1
emirhanyener <yeneremirhann@gmail.com>
2022-11-18 19:18:56 +0300

    test canvas drawing

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("application");
+var ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.fillStyle = "#000000";
+ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+ctx.fillStyle = "#FFFFFF";
+ctx.fillRect(10,10,75,100);
+ctx.font = "20px Arial";
+ctx.fillText("file_name", 10, 140);
