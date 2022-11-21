b7210737b38b07b727e4483384f5525c1919c6f7
emirhanyener <yeneremirhann@gmail.com>
2022-11-21 20:54:04 +0300

    update file_upload: add file upload system

--- file_upload.php
+++ file_upload.php
@@ -1,5 +1,12 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        //upload file
+    error_reporting(E_ERROR | E_PARSE); 
+    session_start(); 
+    if (!file_exists('uploads')) {
+        mkdir('uploads', 0777);
     }
+    $filename = $_SESSION["userid"].'_'.$_FILES['file']['name'];            
+
+    move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/'.$filename);
+      
+    echo 'uploads/'.$filename;
 ?>
