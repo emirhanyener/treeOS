e449b98af45478dd2e85558ee60df1cf0a1cbaeb
emirhanyener <yeneremirhann@gmail.com>
2022-11-28 21:09:59 +0300

    update appscript: add pointer info

--- js/appscript.php
+++ js/appscript.php
@@ -3,6 +3,12 @@
     var canvas = document.getElementById("application");
     var ctx = canvas.getContext("2d");
 
+    var pointer = {
+        is_dragging: false,
+        pointer_position_x: 0,
+        pointer_position_y: 0
+    };
+
     const files = [
         <?php
             foreach($files as $item){
@@ -17,6 +23,11 @@
         ?>
     ];
 
+
+	document.addEventListener('mousemove', pointer_stats);
+	document.addEventListener('mousedown', switch_drag);
+	document.addEventListener('mouseup', switch_drag);
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx.fillStyle = "#000000";
@@ -27,6 +38,15 @@
         ctx.font = "20px Arial";
         ctx.fillText(item.filename, item.position_x, item.position_y + 130);
     });
+        
+    function pointer_stats(e){
+        pointer.pointer_position_x = e.clientX;
+        pointer.pointer_position_y = e.clientY;
+        console.log(pointer);
+    }
+    function switch_drag(){
+        pointer.is_dragging = !pointer.is_dragging;
+    }
     
     function drop(e){
         e.preventDefault();
