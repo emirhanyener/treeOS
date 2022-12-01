d6658cacb8c79e184e65842cac8995a5ad109d28
emirhanyener <yeneremirhann@gmail.com>
2022-12-01 09:24:10 +0300

    update desktop: add double click function to app

--- desktop.php
+++ desktop.php
@@ -8,7 +8,7 @@
         if(isset($_SESSION["login"])){
             $files = $db->query("select * from files where user_id={$_SESSION['userid']}");
     ?>
-        <form style="margin:0px;" action="file_upload.php" ondrop="drop(event)" ondragover="allow_drop(event)" method="post">
+        <form style="margin:0px;" action="file_upload.php" ondrop="drop(event)" ondragover="allow_drop(event)" ondblclick="open_file()" method="post" enctype="multipart/form-data">
             <canvas id="application"></canvas>
         </form>
     <?php
