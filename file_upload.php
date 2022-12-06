b700098e57cb54124bf42417c37fca931d5beaa7
emirhanyener <yeneremirhann@gmail.com>
2022-12-06 18:07:22 +0300

    appscript: add time to uploaded file name

--- file_upload.php
+++ file_upload.php
@@ -5,7 +5,7 @@
     if (!file_exists('uploads')) {
         mkdir('uploads', 0777);
     }
-    $filename = $_SESSION["userid"].'_'.$_FILES['file']['name'];
+    $filename = $_SESSION["userid"].'_'.time().'_'.$_FILES['file']['name'];
 
     $query = $db->query("INSERT INTO files (file_name,position_x,position_y,user_id) VALUES ('".$filename."',100,100,'".$_SESSION["userid"]."')", PDO::FETCH_ASSOC);
 
