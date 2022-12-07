576ef94e506331613147b48b175eb96dd9409e22
emirhanyener <yeneremirhann@gmail.com>
2022-12-07 15:13:28 +0300

    fix footer

--- footer.php
+++ footer.php
@@ -7,11 +7,10 @@
             <img src="images/logo.png" height="100" class="" alt="logo">
         </a>
     </div>
-        <div>
-            <a href="index.php" class="nav-link px-2 text-muted">Home</a>
-            <a href="login.html" class="nav-link px-2 text-muted">Login</a>
-            <a href="register.html" class="nav-link px-2 text-muted">Register</a>
-            <a href="desktop.html" class="nav-link px-2 text-muted">Desktop</a>
-        </div>
     <div>
+        <a href="index.php" class="nav-link px-2 text-muted">Home</a>
+        <a href="login.html" class="nav-link px-2 text-muted">Login</a>
+        <a href="register.html" class="nav-link px-2 text-muted">Register</a>
+        <a href="desktop.html" class="nav-link px-2 text-muted">Desktop</a>
+    </div>
 </div>
