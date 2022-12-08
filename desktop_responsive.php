4321ed3f10a39a4a2bc03e8abb1486ce0416fb7d
emirhanyener <yeneremirhann@gmail.com>
2022-12-08 18:41:04 +0300

    add responsive desktop php

---
+++ desktop_responsive.php
@@ -0,0 +1,45 @@
+<?php $title = "Desktop Responsive"; ?>
+<html>
+    <?php include("head.php"); ?>
+    <body>
+        <div class="container">
+            <?php include("menu.php"); ?>    
+            <?php
+                if(isset($_SESSION["login"])){
+                    $files = $db->query("select * from files where user_id={$_SESSION['userid']}");
+                    $user = $db->query("select * from users where id={$_SESSION['userid']}")->fetch(PDO::FETCH_ASSOC);
+            ?>
+                <div id="body">
+                    <h2><?php echo $user["username"]; ?>'s files</h2>
+                    <table>
+                    <tr>
+                        <th>File Name</th>
+                        <th>Download</th>
+                    </tr>
+                    <?php
+                        foreach($files as $item){
+                            if($item["is_folder"] == 1)
+                                continue;
+                    ?>
+                    <tr>
+                        <td><?php echo $item['file_name']; ?></td>
+                        <td><a href="<?php echo 'uploads/'.$item['file_name']; ?>" download>Download File</a></td>
+                    </tr>
+                    <?php
+                        }
+                    ?>
+                    
+                    </table>
+                <?php
+                    } else {
+                ?>
+                <div class="container">
+                    <p>For using desktop please <a href="login.php">login</a></p>
+                </div>
+                <?php
+                    }
+                ?>
+            <?php include("footer.php"); ?>
+        </div>
+    </body>
+</html>
