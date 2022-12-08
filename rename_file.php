c270216ab7f86d5d7d981383936b2f905effc006
emirhanyener <yeneremirhann@gmail.com>
2022-12-08 22:04:20 +0300

    fix rename

--- rename_file.php
+++ rename_file.php
@@ -2,9 +2,13 @@
     include("db.php");
     ob_start();
     session_start();
-    $newfilename = $_SESSION["userid"].'_'.time().'_'.$_POST["tofilename"];
-    if(file_exists("uploads/".$_POST["filename"]))
-        rename("uploads/".$_POST["filename"], "uploads/".$newfilename);
+    if($_POST["isfolder"] == 0) {
+        $newfilename = $_SESSION["userid"].'_'.time().'_'.$_POST["tofilename"];
+        if(file_exists("uploads/".$_POST["filename"]))
+            rename("uploads/".$_POST["filename"], "uploads/".$newfilename);
+    } else {
+        $newfilename = $_POST["tofilename"];
+    }
     $sql = $db->query("update files set file_name='".$newfilename."' where file_name='".$_POST["filename"]."'");
     header('Location: desktop.php');
 ?>
