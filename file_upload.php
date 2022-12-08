0a36240c4091674b49b3948e5d87969830c1788a
emirhanyener <yeneremirhann@gmail.com>
2022-12-08 17:52:15 +0300

    fix file upload

--- file_upload.php
+++ file_upload.php
@@ -7,7 +7,7 @@
     }
     $filename = $_SESSION["userid"].'_'.time().'_'.$_FILES['file']['name'];
 
-    $query = $db->query("INSERT INTO files (file_name,position_x,position_y,user_id,foldername) VALUES ('".$filename."',".$_POST["position_x"].",".$_POST["position_y"].",'".$_SESSION["userid"]."','')", PDO::FETCH_ASSOC);
+    $query = $db->query("INSERT INTO files (file_name,position_x,position_y,user_id,foldername,is_folder) VALUES ('".$filename."',".$_POST["position_x"].",".$_POST["position_y"].",'".$_SESSION["userid"]."','',0)", PDO::FETCH_ASSOC);
 
     move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/'.$filename);
       
