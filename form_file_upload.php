7ee5555fb5d087428fce66af36611b6cd6bf7647
emirhanyener <yeneremirhann@gmail.com>
2022-12-16 23:28:37 +0300

    add form file upload php

---
+++ form_file_upload.php
@@ -0,0 +1,16 @@
+<?php
+    include("db.php");
+    error_reporting(E_ERROR | E_PARSE); 
+    session_start(); 
+    if (!file_exists('uploads')) {
+        mkdir('uploads', 0777);
+    }
+
+    $filename = $_SESSION["userid"].'_'.time().'_'.$_FILES['file']['name'];
+
+    $query = $db->query("INSERT INTO files (file_name,position_x,position_y,user_id,foldername,is_folder) VALUES ('".$filename."',100 ,100 ,'".$_SESSION["userid"]."','',0)", PDO::FETCH_ASSOC);
+
+    move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/'.$filename);
+
+    header("Location: desktop_responsive.php");
+?>
