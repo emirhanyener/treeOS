3487c2107a36e277bbfe83caf3edd387cb2894a0
emirhanyener <yeneremirhann@gmail.com>
2023-01-08 19:35:21 +0300

    fix delete file

--- delete_file.php
+++ delete_file.php
@@ -2,5 +2,12 @@
     include("db.php");
     if(file_exists("uploads/".$_POST["filename"]))
         unlink("uploads/".$_POST["filename"]);
-    $sql = $db->query("delete from files where file_name='".$_POST["filename"]."'");
+    if (explode(".", $_POST["filename"])[1] == "png" || explode(".", $_POST["filename"])[1] == "jpg") {
+        $sql_select = $db->query("select * from users where background_image ='" . $_POST["filename"] . "'");
+        if ($sql_select->rowCount() == 1) {
+            $sql_delete = $db->query("update users set background_image = '' where background_image='" . $_POST["filename"] . "'");
+        }
+    }
+    if($db->query("select * from files where foldername='".$_POST["filename"]."'")->rowCount() == 0)
+        $sql_delete = $db->query("delete from files where file_name='".$_POST["filename"]."'");
 ?>
