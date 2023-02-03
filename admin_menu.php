1302474adc0ef95df889286c7d13cf5ad9308199
emirhanyener <yeneremirhann@gmail.com>
2023-02-03 22:56:32 +0300

    add admin menu

---
+++ admin_menu.php
@@ -0,0 +1,16 @@
+
+    <font style="position: fixed; bottom: 5px;right:5px;color:red;"><b>ADMIN PAGE</b></font>
+    <div class="menu">
+        <div class="menu-item" style="position:relative;">
+            <a href="index.php" style="height:100px;" id="logo">
+                <img src="images/logo.png" height="100" class="" alt="logo">
+            </a>
+        </div>
+        <div class="menu-item">
+            <a class="menu-item-right" style="color:white;background-color: #a31313;border-radius: 45px;" href="logout.php">Logout</a>
+            <?php
+            ?>
+                <a class="menu-item-right" href="admin_users.php">Users</a>
+                <a class="menu-item-right" href="admin_files.php">Files</a>
+        </div>
+    </div>
