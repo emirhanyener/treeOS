d547147eeae9183767a79b300265c7bbf74462a0
emirhanyener <yeneremirhann@gmail.com>
2023-02-04 16:37:30 +0300

    add admin delete file

---
+++ admin_delete_file.php
@@ -0,0 +1,8 @@
+<?php
+    include("db.php");
+    if(file_exists("uploads/".$_GET["filename"]))
+        unlink("uploads/".$_GET["filename"]);
+    $sql = $db->query("delete from files where file_name='".$_GET["filename"]."'");
+
+    header("Location: admin_files.php");
+?>
