994d2fb730034044897730a657a04fcc8875b81c
emirhanyener <emirhan-yener@hotmail.com>
2023-09-14 18:12:54 +0300

    add setup.php file

---
+++ setup.php
@@ -0,0 +1,31 @@
+<?php
+    include("config.php"); 
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=".$database_name, $username, $password);
+        $db->query("CREATE TABLE `files` (
+            `user_id` int(11) NOT NULL,
+            `file_name` varchar(50) NOT NULL,
+            `position_x` int(11) NOT NULL,
+            `position_y` int(11) NOT NULL,
+            `foldername` varchar(50) NOT NULL,
+            `is_folder` int(11) NOT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;");
+        $db->query("CREATE TABLE `users` (
+            `id` int(11) NOT NULL,
+            `username` varchar(100) NOT NULL,
+            `password` varchar(100) NOT NULL,
+            `mail` varchar(100) NOT NULL,
+            `is_admin` int(11) NOT NULL,
+            `mail_active` int(11) NOT NULL,
+            `mail_verification` varchar(100) NOT NULL,
+            `desktop_name` varchar(50) NOT NULL,
+            `background_color` varchar(20) NOT NULL,
+            `background_image` varchar(100) NOT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;");
+        $db->query("ALTER TABLE `users` ADD PRIMARY KEY (`id`);");
+        $db->query("ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;");
+        echo "Database created!";
+    } catch (PDOException $e) {
+        echo "Database already created!";
+    }
+?>
